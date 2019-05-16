d79e6bbd03e4d8f6c9d793e1f46c176cdd9cd12a
Enrique Pérez Arnaud <enrique@cazalla.net>
2019-05-16 14:51:51 +0200

    wrong year

--- src/eduid_webapp/idp/__init__.py
+++ src/eduid_webapp/idp/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2016 NORDUnet A/S
+# Copyright (c) 2019 NORDUnet A/S
 # All rights reserved.
 #
 #   Redistribution and use in source and binary forms, with or
