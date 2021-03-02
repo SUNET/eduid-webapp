81897dc801753f388ebc5173555e73e3b8c16dc5
Fredrik Thulin <fredrik@thulin.net>
2021-03-02 14:53:38 +0100

    use pip-tools to manage requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 import os
+from pathlib import PurePath
+from typing import List
+
 from setuptools import setup, find_packages
 
 version = '0.2.27'
 
-here = os.path.abspath(os.path.dirname(__file__))
 
-install_requires = [x for x in open(os.path.join(here, 'requirements.txt')).read().split('\n') if len(x) > 0]
-testing_extras = [x for x in open(os.path.join(here, 'test_requirements.txt')).read().split('\n')
-                  if len(x) > 0 and not x.startswith('-')]
+def load_requirements(path: PurePath) -> List[str]:
+    """ Load dependencies from a requirements.txt style file, ignoring comments etc. """
+    res = []
+    with open(path) as fd:
+        for line in fd.readlines():
+            while line.endswith('\n') or line.endswith('\\'):
+                line = line[:-1]
+            line = line.strip()
+            if not line or line.startswith('-') or line.startswith('#'):
+                continue
+            res += [line]
+    return res
+
+
+here = PurePath(__file__)
+README = open(here.with_name('README.txt')).read()
+
+install_requires = load_requirements(here.with_name('requirements.txt'))
+test_requires = load_requirements(here.with_name('test_requirements.txt'))
 
 setup(
     name='eduid-webapp',
@@ -18,16 +36,13 @@ setup(
     author='SUNET',
     author_email='',
     description='web apps for eduID',
-    classifiers=[
-        'Framework :: Flask',
-    ],
+    classifiers=['Framework :: Flask',],
     packages=find_packages('src'),
     package_dir={'': 'src'},
     namespace_packages=['eduid_webapp'],
     zip_safe=False,
     include_package_data=True,
     install_requires=install_requires,
-    extras_require={
-        'testing': testing_extras,
-    },
+    test_requires=test_requires,
+    extras_require={'testing': []},
 )
